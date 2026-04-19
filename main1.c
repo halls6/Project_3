102204cab35beb85af5b7258f1cd80e6485f0bff
Sofia <halls6@tcnj.edu>
2026-04-19 19:02:35 -0400

    checking TLB in main1.c

--- main1.c
+++ main1.c
@@ -18,7 +18,7 @@ int TLBNext = 0;
 
 /* counters */
 int pageFaults = 0;
-int tableHits = 0;
+int TLBHits = 0;
 int nextFram = 0;
 int total = 0;
 
@@ -43,15 +43,23 @@ int main(int argc, char *argv[]) {
     FILE *out1 = fopen("out1.txt", "w");
     FILE *out2 = fopen("out2.txt", "w");
     FILE *out3 = fopen("out3.txt", "w");
-    
-    int addr;
 
-    while (fscanf(address, "%d", &addr) == 1) {
-        int masked = addr & 0xFFFF; /* rightmost 16 bits masked */
+    int logAddr;
 
+    while (fscanf(address, "%d", &logAddr) == 1) {
+        total++;
+        int masked = logAddr & 0xFFFF; /* rightmost 16 bits masked */
         int pageNum = (masked >> 8) & 0xFF; /* get page number */
-
         int offset = masked & 0xFF; /* get offset */
+        int frameNum = -1;
+
+        for (i = 0; i < TLB_SIZE; i++) {
+            if (TLBPage[i] == pageNum) {
+                frameNum = TLBFrame[i];
+                TLBHits++;
+                break;
+            }
+        }
 
         /* output to terminal */
         printf("Logical: %d, Page: %d, Offset: %d\n", masked, pageNum, offset);
