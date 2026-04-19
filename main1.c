012cb05d50d29ea8e468f330f58281e3e01ffdd9
Sofia <halls6@tcnj.edu>
2026-04-19 18:45:03 -0400

    adding counters, constants, tables, etc.

--- main1.c
+++ main1.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+/* constants */
+#define PAGE_SIZE 256
+#define FRAME_COUNT 256
+#define TABLE_SIZE 16
+#define PAGE_COUNT 256
+
+signed char physicalMem[FRAME_COUNT][PAGE_SIZE]; /* physical memory */
+
+/* tables */
+int pageTable[PAGE_COUNT]; 
+int tablePage[TABLE_SIZE];
+int tableFrame[TABLE_SIZE];
+int tableNext = 0;
+
+/* counters */
+int pageFaults = 0;
+int tableHits = 0;
+int nextFram = 0;
+int total = 0;
 
 /* main function */
 int main(int argc, char *argv[]) {
