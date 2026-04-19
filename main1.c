cca6cbb1957d6e0480519234e3814905abe435fe
Sofia <halls6@tcnj.edu>
2026-04-19 18:59:20 -0400

    reading in files and output files in main1.c

--- main1.c
+++ main1.c
@@ -4,16 +4,17 @@
 /* constants */
 #define PAGE_SIZE 256
 #define FRAME_COUNT 256
-#define TABLE_SIZE 16
+#define TLB_SIZE 16
 #define PAGE_COUNT 256
 
 signed char physicalMem[FRAME_COUNT][PAGE_SIZE]; /* physical memory */
 
-/* tables */
+/* arrays */
 int pageTable[PAGE_COUNT]; 
-int tablePage[TABLE_SIZE];
-int tableFrame[TABLE_SIZE];
-int tableNext = 0;
+int TLBPage[TLB_SIZE];
+int TLBFrame[TLB_SIZE];
+
+int TLBNext = 0;
 
 /* counters */
 int pageFaults = 0;
@@ -23,8 +24,26 @@ int total = 0;
 
 /* main function */
 int main(int argc, char *argv[]) {
-    FILE *address = fopen(argv[1], "r"); /* opening addresses.txt */
+    int i;
 
+    /* page table and TLB initialized to -1 */
+    for (int i = 0; i < PAGE_COUNT; i++) {
+        pageTable[i] = -1;
+    }
+    for (int i = 0; i < TLB_SIZE; i++) {
+        TLBPage[i] = -1;
+        TLBFrame[i] = -1;
+    }
+
+    /* reading in files */
+    FILE *address = fopen(argv[1], "r"); /* opening addresses.txt */
+    FILE *backing = fopen("BACKING_STORE.bin", "rb"); /* opening backing file */
+    
+    /* output files */
+    FILE *out1 = fopen("out1.txt", "w");
+    FILE *out2 = fopen("out2.txt", "w");
+    FILE *out3 = fopen("out3.txt", "w");
+    
     int addr;
 
     while (fscanf(address, "%d", &addr) == 1) {
